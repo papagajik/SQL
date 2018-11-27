2ac6b75f7028493cc02e2d7b7d3598cfc67c024e
papagajik <papagajik@gmail.com>
2018-11-27 14:09:54 +0100

    Create test1.sql

---
+++ test1.sql
@@ -0,0 +1,4 @@
+select * from ser_z_l l where l.rid = '49318000-62';
+select * from ser_z_tasks szt where szt.rid_o = '49318000-62';
+select * from obch_pripady_o o where rid_o  ='4881800024' and id_r = 1;
+select * from t_dokl_l l where l.rid_v_op = '4881800024!1'; 
